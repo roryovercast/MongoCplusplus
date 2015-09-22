2b52b78426fb4ae0ff3430712b0ee3f2f43a57d6
Rory Overcast <rory.overcast@gmail.com>
2015-09-22 17:18:21 -0500

    Initial Commit

---
+++ MongoTesting/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MongoTesting
+//
+//  Created by Rory Overcast on 9/22/15.
+//  Copyright (c) 2015 Rory Overcast. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
